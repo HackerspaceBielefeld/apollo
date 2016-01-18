47aa79b504140fd2de10fa368d7b739d1638502b
electronicfreak <github@electronicfreak.de>
2016-01-18 01:12:24 +0100

    Recherche und Grundgerüst

--- ardoSwitch.ino
+++ ardoSwitch.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Ethernet.h>
+#include <SPI.h>
 
+byte mac[] = { 0x00 , 0xAA , 0xBB , 0xCC , 0xDE , 0x02 }; // macadresse
+
+int maintain = 30; //checke renew alle x sekunden
+
+int m = 0;
+EthernetClient client;
+
+void setup() {
+	// dhcp holen
+	while(Ethernet.begin(mac) == 0) {
+		delay(1000);
+	}
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+	if(maintain > m) {
+		m++;
+	}else{
+		// renew falls nötig
+		Ethernet.maintain();
+		m = 0;
+	}
+	
+	
+  
+	// etwas bremsen
+	delay(1000);
 }
