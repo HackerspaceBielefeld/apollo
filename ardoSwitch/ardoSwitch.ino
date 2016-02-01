51d67afb897db2eab7b61e396285bc8392366158
electronicfreak <github@electronicfreak.de>
2016-02-01 20:57:09 +0100

    läuft System kann per browser kanäle schalten, toggeln und 100-1000ms anwischen

--- ardoSwitch/ardoSwitch.ino
+++ ardoSwitch/ardoSwitch.ino
@@ -1,9 +1,9 @@
 #include <Ethernet.h>
 #include <SPI.h>
 
-byte mac[] = { 0x00 , 0xAA , 0xBB , 0xCC , 0xDE , 0x02 }; // macadresse
-int dPinO[] = {13}; //digitale Ausgänge
-int dPinI[] = {11}; //digitale Eingänge
+byte mac[] = { 0x00 , 0x00 , 0x00 , 0x82 , 0xB1 , 0x73 }; // macadresse
+int dPinO[] = {2,3,4,5,6,7,8,9,10,11,12,13}; //digitale Ausgänge
+int dPinI[] = {}; //digitale Eingänge
 
 int aPinO[] = {}; //analoge Ausgänge (PWM)
 int aPinI[] = {}; //analoge Eingänge
@@ -16,41 +16,41 @@ int m = 0;
 EthernetServer server(80);
 
 bool isIn(int arr[],int needle) {
-	for(int i=0;i<sizeof(arr);i++) {
-		if(arr[i] == needle) return true;
-	}
-	return false;
+  for(int i=0;i<sizeof(arr)/2;i++) {
+    if(arr[i] == needle) return true;
+  }
+  return false;
 }
 
-int dPin(bool mode,int pin,bool val=false) {
-	//return 0 pin = false; 1 pin = true; 2 chance succeded; 3 pin not listed; 4 error
-	//mode false getPin; true changePin
-	if(mode) {
-		if(isIn(dPinO,pin)){
-			digitalWrite(pin,val);
+int dPin(bool mode,int pin,int val=false) {
+  //return 0 pin = false; 1 pin = true; 2 chance succeded; 3 pin not listed; 4 error
+  //mode false getPin; true changePin
+  if(mode) {
+    if(isIn(dPinO,pin)){
+      digitalWrite(pin,val);
       Serial.print(pin);
       Serial.print(" auf ");
       Serial.print(val);
       Serial.println(" schalten.");
-			return 2;
-		}else{
+      return 2;
+    }else{
       Serial.print(pin);
       Serial.println(" Nicht in der Liste.");
-			return 3;
-		}
-	}else{
-		if(isIn(dPinI,pin) || isIn(dPinO,pin)) {
+      return 3;
+    }
+  }else{
+    if(isIn(dPinI,pin) || isIn(dPinO,pin)) {
       Serial.print(pin);
       Serial.println(" wird abgefragt.");
-			return digitalRead(pin);
-		}
-	}
-	return 4;
+      return digitalRead(pin);
+    }
+  }
+  return 4;
 }
 
 //TODO
 int aPin() {
-	
+  
 }
 
 void printIPAddress() {
@@ -70,99 +70,176 @@ void setup() {
   }
   Serial.println("Apollo start ...");
   
-	// IOs definieren
-	for(int i = 0;i<sizeof(dPinI);i++) {
-		pinMode(dPinI[i],INPUT_PULLUP);
+  // IOs definieren  
+  for(int i = 0;i<sizeof(dPinI)/2;i++) {
+    pinMode(dPinI[i],INPUT_PULLUP);
     Serial.print(dPinI[i]);
     Serial.println(" wird als Eingang definiert.");
-	}
-	for(int i = 0;i<sizeof(dPinO);i++) {
-		pinMode(dPinO[i],OUTPUT);
-    Serial.print(dPinO[i]);
+  }
+  for(int j = 0;j<sizeof(dPinO)/2;j++) {
+    if(dPinO[j] == 0 ) break;
+    pinMode(dPinO[j],OUTPUT);
+    Serial.print(dPinO[j]);
     Serial.println(" wird als Ausgang definiert.");
-	}
-	
-	// dhcp holen
+  }
+  
+  // dhcp holen
  
   Serial.println("DHCP anfrage.");
-	while(Ethernet.begin(mac) == 0) {
-		delay(1000);
-	}
+  while(Ethernet.begin(mac) == 0) {
+    delay(1000);
+  }
   printIPAddress();
-	// server starten
-	server.begin();
+  // server starten
+  server.begin();
 }
 
 void loop() {
-	if(maintain > m) {
-		m++;
-	}else{
-		// renew falls nötig
-		Ethernet.maintain();
-		m = 0;
-	}
+  if(maintain > m) {
+    m++;
+  }else{
+    // renew falls nötig
+    Ethernet.maintain();
+    m = 0;
+  }
 
-	// auf anfrage Prüfen
-	EthernetClient client = server.available();
-	if (client) {
-		String req = "";
-		boolean currentLineIsBlank = true;
-		while (client.connected()) {
-			if (client.available()) {
-				char c = client.read();
-				if (c == '\n' && currentLineIsBlank) {
-					// standard antwort header
-					client.println("HTTP/1.1 200 OK");
-					client.println("Content-Type: text/html");
-					client.println("Connection: close");
-					client.println();
-					// content
-					client.println(4);
-					// content ende
-					break;
-				}
-				if (c == '\n') {
-					if(req.substring(0,3) == "GET"){
-						int index = req.indexOf("/");
-						req = req.substring(index+1);
-						index = req.indexOf("/");
-            int index2 = req.indexOf(" ");
-						if(index < index2) {
-							int pin = req.substring(0,index).toInt();
-							req = req.substring(index);
-              Serial.println(req);
-							index = req.indexOf(" ");
-							if(index >= 0) {
-                Serial.print("ind: ");
-                Serial.println(index);
-								int val = req.substring(1,index).toInt();
-                Serial.print("Val: ");
-                Serial.println(val);
-								client.println(dPin(true,pin,val));
-							}else{
-								client.println(dPin(false,pin));
-							}
-						}else{
-							int pin = req.substring(0,index2).toInt();
-							client.println(dPin(false,pin));
-						}
-						break;
-					}
-					req = "";
-					currentLineIsBlank = true;
-				} else if (c != '\r') {
-					// buchstabe sammeln
-					req.concat(c);
-					currentLineIsBlank = false;
-				}
-			}
-		}
+  // auf anfrage Prüfen
+  EthernetClient client = server.available();
 
-		delay(1);
-		// close the connection:
-		client.stop();
-	}
+  if (client) {
+    String req = "";
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        if (c == '\n' && currentLineIsBlank) {
+          // standard antwort header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");
+          client.println();
+          // content
+          client.print(4);
+          // content ende
+          break;
+        }
+        if (c == '\n') {
+          if(req.substring(0,3).compareTo("GET") == 0){
+            //Serial.println(req);
+            int index = req.indexOf("/");
+            int index2;
+            int pin;
+            int val;
+            req = req.substring(index+1);
+            index = req.indexOf("/");
+            index2 = req.indexOf(" ");
+            //Serial.print("Index: ");
+            //Serial.println(index);
+            //Serial.print("Index2: ");
+            //Serial.println(index2);
+            
+            if(index < index2) {
+              // kanal ändern
+              //Serial.println(req.substring(0,index));
+              pin = req.substring(0,index).toInt();
+              if(isIn(dPinO,pin)) { 
+                client.println(3);
+                break;
+                // nicht listed
+              }
+              req = req.substring(index+1);
+              index = req.indexOf("/");
+              index2 = req.indexOf(" ");
+              if(index > index2) {
+                index = index2;
+              }
+              //Serial.print("Index: ");
+              //Serial.println(index);
+              //Serial.println(req);
+              //Serial.println(req.substring(0,index));
+              val = req.substring(0,index).toInt();
+              
+              if(val == 0) {
+                // ausschalten
+                digitalWrite(pin,LOW);
+                client.println(2);
+                Serial.print(pin);
+                Serial.println(" schaltet LOW.");
+                break; 
+              }else if(val == 1) {
+                // einschalten
+                digitalWrite(pin,HIGH);
+                client.println(2);
+                Serial.print(pin);
+                Serial.println(" schaltet HIGH.");
+                break;
+              }else if(val == 2) {
+                // toggle 
+                int tmp = digitalRead(pin);
+                if(tmp == HIGH) {
+                  digitalWrite(pin,LOW);
+                  client.println(2);
+                  Serial.print(pin);
+                  Serial.println(" toggelt LOW.");
+                }else{
+                  digitalWrite(pin,HIGH);
+                  client.println(2);
+                  Serial.print(pin);
+                  Serial.println(" toggelt HIGH.");
+                }
+              }else if(val > 99 && val <=1000) {
+                // 0.2sek toggle
+                int tmp = digitalRead(pin);
+                if(tmp == HIGH) {
+                  digitalWrite(pin,LOW);
+                  delay(val);
+                  digitalWrite(pin,HIGH);
+                  client.println(2);
+                  Serial.print(pin);
+                  Serial.println(" toggelt LOW.");
+                }else{
+                  digitalWrite(pin,HIGH);
+                  delay(val);
+                  digitalWrite(pin,LOW);
+                  client.println(2);
+                  Serial.print(pin);
+                  Serial.println(" toggelt HIGH.");
+                }
+              }else{
+                client.println(4);
+                Serial.print(val);
+                Serial.println(" ist ungültig");
+                break; 
+              }
+              
+            }else{
+              // kanal auslesen
+              Serial.println(req.substring(0,index2));
+              pin = req.substring(0,index2).toInt();
+              index = index2;
+            }
+            Serial.print("Pin: ");
+            Serial.println(pin);
+            Serial.print("Val: ");
+            Serial.println(val);
+            
+            break;
+          }
+          req = "";
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // buchstabe sammeln
+          req.concat(c);
+          currentLineIsBlank = false;
+        }
+      }
+    }
 
-	// etwas bremsen
-	delay(999);
+    delay(1);
+    // close the connection:
+    client.stop();
+  }
+
+  // etwas bremsen
+  delay(999);
 }
