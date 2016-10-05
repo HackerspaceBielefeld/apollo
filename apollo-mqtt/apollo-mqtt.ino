7b8d5a19af9f28fb5eecab2550bf043daa82af20
electronicfreak <github@electronicfreak.de>
2016-10-05 14:15:36 +0200

    ign und mqtt aber ungetestet

--- apollo-mqtt/apollo-mqtt.ino
+++ apollo-mqtt/apollo-mqtt.ino
@@ -1,15 +1,21 @@
 #include <Ethernet.h>
 #include <SPI.h>
 #include <Wire.h>
-#include "Adafruit_MCP23017.h"
+#include <Adafruit_MCP23017.h>
 #include <PubSubClient.h>
 
 byte mac[] = { 0x00 , 0x00 , 0x00 , 0x82 , 0xB1 , 0x73 }; // macadresse
 
 int maintain = 30; //checke renew alle x sekunden
+
+char server[] = "172.23.45.5";
+
 String devName = "apollo";
-String topicDebug = "debug/apollo";
-String topicInput[] = {"apollo/?","light/schankraum/?"}
+char* topicDebug = "debug/apollo";
+int topicCount = 1;
+char* topicInput[] = {"apollo/?"};
+
+
 
 // --------------------------------------------------------------------------
 
@@ -17,14 +23,143 @@ int m = 0;
 
 Adafruit_MCP23017 mcp; //port extender
 EthernetClient ethClient;
-PubSubClient mqtt(ethClient);
+PubSubClient mqtt;
 
 void callback(char* topic, byte* pl, unsigned int length) {
 	char payload[length];
 	for (int i=0;i<length;i++) {
-		Serial.print((char)payload[i]);
+    payload[i] = (char)pl[i];  
 	}
-	// TODO
+  Serial.print(payload);
+
+  if(topic == "apollo/0") {
+    if(payload == "ON") {
+      mcp.digitalWrite(0, HIGH);
+    }else{
+      mcp.digitalWrite(0, LOW);
+    }
+  }
+
+  if(topic == "apollo/1") {
+    if(payload == "ON") {
+      mcp.digitalWrite(1, HIGH);
+    }else{
+      mcp.digitalWrite(1, LOW);
+    }
+  }
+
+  if(topic == "apollo/2") {
+    if(payload == "ON") {
+      mcp.digitalWrite(2, HIGH);
+    }else{
+      mcp.digitalWrite(2, LOW);
+    }
+  }
+
+  if(topic == "apollo/3") {
+    if(payload == "ON") {
+      mcp.digitalWrite(3, HIGH);
+    }else{
+      mcp.digitalWrite(3, LOW);
+    }
+  }
+
+  if(topic == "apollo/4") {
+    if(payload == "ON") {
+      mcp.digitalWrite(4, HIGH);
+    }else{
+      mcp.digitalWrite(4, LOW);
+    }
+  }
+
+  if(topic == "apollo/5") {
+    if(payload == "ON") {
+      mcp.digitalWrite(5, HIGH);
+    }else{
+      mcp.digitalWrite(5, LOW);
+    }
+  }
+
+  if(topic == "apollo/6") {
+    if(payload == "ON") {
+      mcp.digitalWrite(6, HIGH);
+    }else{
+      mcp.digitalWrite(6, LOW);
+    }
+  }
+
+  if(topic == "apollo/7") {
+    if(payload == "ON") {
+      mcp.digitalWrite(7, HIGH);
+    }else{
+      mcp.digitalWrite(7, LOW);
+    }
+  }
+
+  if(topic == "apollo/8") {
+    if(payload == "ON") {
+      mcp.digitalWrite(8, HIGH);
+    }else{
+      mcp.digitalWrite(8, LOW);
+    }
+  }
+
+  if(topic == "apollo/9") {
+    if(payload == "ON") {
+      mcp.digitalWrite(9, HIGH);
+    }else{
+      mcp.digitalWrite(9, LOW);
+    }
+  }
+
+  if(topic == "apollo/10") {
+    if(payload == "ON") {
+      mcp.digitalWrite(10, HIGH);
+    }else{
+      mcp.digitalWrite(10, LOW);
+    }
+  }
+
+  if(topic == "apollo/11") {
+    if(payload == "ON") {
+      mcp.digitalWrite(11, HIGH);
+    }else{
+      mcp.digitalWrite(11, LOW);
+    }
+  }
+
+  if(topic == "apollo/12") {
+    if(payload == "ON") {
+      mcp.digitalWrite(12, HIGH);
+    }else{
+      mcp.digitalWrite(12, LOW);
+    }
+  }
+
+  if(topic == "apollo/13") {
+    if(payload == "ON") {
+      mcp.digitalWrite(13, HIGH);
+    }else{
+      mcp.digitalWrite(13, LOW);
+    }
+  }
+
+  if(topic == "apollo/14") {
+    if(payload == "ON") {
+      mcp.digitalWrite(14, HIGH);
+    }else{
+      mcp.digitalWrite(14, LOW);
+    }
+  }
+
+  if(topic == "apollo/15") {
+    if(payload == "ON") {
+      mcp.digitalWrite(15, HIGH);
+    }else{
+      mcp.digitalWrite(15, LOW);
+    }
+  }
+  
 	Serial.print("Message arrived [");
 	Serial.print(topic);
 	Serial.print("] ");
@@ -52,24 +187,27 @@ void printIPAddress() {
 	strcat(ip,".");
 	strcpy(ip,(int)Ethernet.localIP()[3]);
 	
-	client.publish(topicDebug,ip)
+	mqtt.publish(topicDebug,ip);
 	Serial.println("");
 }
+
 
 //verbindet mqtt neu zum broker
 //ungetestet
 void reconnect() {
-	while (!client.connected()) {
+	while (!mqtt.connected()) {
 		Serial.print("Attempting MQTT connection...");
-		if (client.connect("arduinoClient")) {
+		if (mqtt.connect("arduinoClient")) {
 			Serial.println("connected");
-			client.publish(topicDebug,"reconnected");
-			client.subscribe("inTopic");
+			mqtt.publish(topicDebug,"reconnected");
+      for(int i=0; i<topicCount; i++) {
+        mqtt.subscribe(topicInput[i]);
+      }
 		} else {
 			Serial.print("failed, rc=");
-			Serial.print(client.state());
+			Serial.print(mqtt.state());
 			Serial.println(" try again in 5 seconds");
-			client.publish(topicDebug,"connection lost");
+			//mqtt.publish(topicDebug,"connection lost");
 			delay(5000);
 		}
 	}
@@ -82,11 +220,28 @@ void setup() {
 	}
 	Serial.println("Apollo start ...");
 	
-	mqtt.setServer(server, 1883); //mqtt starten
+	mqtt.setClient(ethClient);
+  mqtt.setServer(server, 1883); //mqtt starten
 	mqtt.setCallback(callback);
 	delay(1500);
 
 	mcp.begin(); // port extender starten
+  mcp.pinMode(0, OUTPUT);
+  mcp.pinMode(1, OUTPUT);
+  mcp.pinMode(2, OUTPUT);
+  mcp.pinMode(3, OUTPUT);
+  mcp.pinMode(4, OUTPUT);
+  mcp.pinMode(5, OUTPUT);
+  mcp.pinMode(6, OUTPUT);
+  mcp.pinMode(7, OUTPUT);
+  mcp.pinMode(8, OUTPUT);
+  mcp.pinMode(9, OUTPUT);
+  mcp.pinMode(10, OUTPUT);
+  mcp.pinMode(11, OUTPUT);
+  mcp.pinMode(12, OUTPUT);
+  mcp.pinMode(13, OUTPUT);
+  mcp.pinMode(14, OUTPUT);
+  mcp.pinMode(15, OUTPUT);
 
 	// dhcp holen
 	Serial.println("DHCP anfrage.");
